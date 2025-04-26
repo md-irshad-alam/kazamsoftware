e3df943151425705ef8c9be0e68cceaa20ab1753
irshad <almairsh0205@gmail.com>
2025-04-26 12:55:09 +0530

    Mongodb altas uir added

--- backend/controller/db.ts
+++ backend/controller/db.ts
@@ -4,10 +4,14 @@ const mongoose = require("mongoose");
 
 const connection = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(
+      `mongodb+srv://${process.env.DB_URI_USER}:${process.env.DB_URI_PASS}@cluster0.eecjhwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log(
       `✅ MongoDB Connected: ${db.connection.host}/${db.connection.name}`
     );
+
+    console.log("Connection established successfully.");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed!");
     console.error(`Error: ${error}`);
